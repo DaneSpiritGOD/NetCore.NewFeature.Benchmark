50f215543bf749453fdab4a92f1802048c677c90
Dane <838652739@qq.com>
2018-11-15 21:39:46 +0800

    init

---
+++ NetCore.NewFeature.Benchmark/Copy.cs
@@ -0,0 +1,131 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+
+namespace NetCore.NewFeature.Benchmark
+{
+    [CoreJob]
+    [RankColumn, CategoriesColumn]
+    [GroupBenchmarksBy(BenchmarkLogicalGroupRule.ByCategory, BenchmarkLogicalGroupRule.ByParams)]
+    [Orderer(BenchmarkDotNet.Order.SummaryOrderPolicy.FastestToSlowest)]
+    public class Copy
+    {
+        private byte[] _managedDataSource;
+        private byte[] _managedDataDestination;
+        private IntPtr _nativeDataSource;
+
+        [Params(16, 32, 64, 2048, 1024 * 1024 * 1024)]
+        public int N;
+        private IntPtr _nativeDataDestination;
+
+        [GlobalSetup]
+        public unsafe void GlobalSetup()
+        {
+            _managedDataSource = new byte[N];
+            new Random(42).NextBytes(_managedDataSource);
+
+            _managedDataDestination = new byte[N];
+
+            _nativeDataSource = Marshal.AllocHGlobal(N);
+            new Random(42).NextBytes(new Span<byte>(_nativeDataSource.ToPointer(), N));
+
+            _nativeDataDestination = Marshal.AllocHGlobal(N);
+        }
+
+        [GlobalCleanup]
+        public void GlobalCleanup()
+        {
+            Marshal.FreeHGlobal(_nativeDataSource);
+            Marshal.FreeHGlobal(_nativeDataDestination);
+        }
+
+        [BenchmarkCategory("ManagedToManaged", "Buffer_MemoryCopy"), Benchmark]
+        public unsafe void ManagedToManaged_Buffer_MemoryCopy()
+        {
+            fixed (byte* pSrc = _managedDataSource, pDest = _managedDataDestination)
+            {
+                Buffer.MemoryCopy(pSrc, pDest, N, N);
+            }
+        }
+
+        [BenchmarkCategory("ManagedToManaged", "Unsafe_CopyBlock"), Benchmark(Baseline = true)]
+        public unsafe void ManagedToManaged_Unsafe_CopyBlock()
+        {
+            fixed (byte* pSrc = _managedDataSource, pDest = _managedDataDestination)
+            {
+                Unsafe.CopyBlock(pDest, pSrc, checked((uint)N));
+            }
+        }
+
+        [BenchmarkCategory("ManagedToManaged", "Array_Copy"), Benchmark]
+        public void ManagedToManaged_Array_Copy()
+        {
+            Array.Copy(_managedDataSource, _managedDataDestination, N);
+        }
+
+        [BenchmarkCategory("NativeToManaged", "Marshal_Copy"), Benchmark]
+        public void NativeToManaged_Marshal()
+        {
+            Marshal.Copy(_nativeDataSource, _managedDataDestination, 0, N);
+        }
+
+        [BenchmarkCategory("NativeToManaged", "Buffer_MemoryCopy"), Benchmark]
+        public unsafe void NativeToManaged_Buffer_MemoryCopy()
+        {
+            fixed (byte* pDest = _managedDataDestination)
+            {
+                Buffer.MemoryCopy(_nativeDataSource.ToPointer(), pDest, N, N);
+            }
+        }
+
+        [BenchmarkCategory("NativeToManaged", "Unsafe_CopyBlock"), Benchmark(Baseline = true)]
+        public unsafe void NativeToManaged_Unsafe_CopyBlock()
+        {
+            fixed (byte* pDest = _managedDataDestination)
+            {
+                Unsafe.CopyBlock(pDest, _nativeDataSource.ToPointer(), checked((uint)N));
+            }
+        }
+
+        [BenchmarkCategory("NativeToNative", "Buffer_MemoryCopy"), Benchmark]
+        public unsafe void NativeToNative_Buffer_MemoryCopy()
+        {
+            Buffer.MemoryCopy(_nativeDataSource.ToPointer(), _nativeDataDestination.ToPointer(), N, N);
+        }
+
+        [BenchmarkCategory("NativeToNative", "Unsafe_CopyBlock"), Benchmark(Baseline = true)]
+        public unsafe void NativeToNative_Unsafe_CopyBlock()
+        {
+            Unsafe.CopyBlock(_nativeDataDestination.ToPointer(), _nativeDataSource.ToPointer(), checked((uint)N));
+        }
+
+        [BenchmarkCategory("ManagedToNative", "Marshal_Copy"), Benchmark]
+        public void ManagedToNative_Marshal_Copy()
+        {
+            Marshal.Copy(_managedDataSource, 0, _nativeDataDestination, N);
+        }
+
+        [BenchmarkCategory("ManagedToNative", "Buffer_MemoryCopy"), Benchmark]
+        public unsafe void ManagedToNative_Buffer_MemoryCopy()
+        {
+            fixed (byte* pSrc = _managedDataSource)
+            {
+                Buffer.MemoryCopy(pSrc, _nativeDataDestination.ToPointer(), N, N);
+            }
+        }
+
+        [BenchmarkCategory("ManagedToNative", "Unsafe_CopyBlock"), Benchmark(Baseline = true)]
+        public unsafe void ManagedToNative_Unsafe_CopyBlock()
+        {
+            fixed (byte* pSrc = _managedDataSource)
+            {
+                Unsafe.CopyBlock(_nativeDataDestination.ToPointer(), pSrc, checked((uint)N));
+            }
+        }
+    }
+}
--- NetCore.NewFeature.Benchmark/Program.cs
+++ NetCore.NewFeature.Benchmark/Program.cs
@@ -1,12 +1,94 @@
-﻿using System;
+﻿using BenchmarkDotNet.Running;
+using System;
+using System.Buffers;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
 
 namespace NetCore.NewFeature.Benchmark
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static unsafe void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var summary = BenchmarkRunner.Run<Copy>();
+            //simpleTest();
+            //Console.ReadKey();
+        }
+
+        private static void display(Span<byte> data, int maxLen = 16)
+        {
+            var lenDisplay = Math.Min(data.Length, maxLen);
+            var sb = new StringBuilder();
+            for (var index = 0; index < lenDisplay; ++index)
+            {
+                sb.Append($"{data[index]} ");
+            }
+            Console.WriteLine(sb.ToString());
+        }
+
+        private static unsafe void simpleTest()
+        {
+            const int Length = 102400000;
+            var ptr = Marshal.AllocHGlobal(Length);
+            var spanSource = new Span<byte>(ptr.ToPointer(), Length);
+            new Random(42).NextBytes(spanSource);
+
+            var dstArray = new byte[Length];
+            var dstArray2 = new byte[Length];
+
+            fixed (byte* p1 = spanSource, p2 = dstArray, p3 = dstArray2)
+            {
+                Buffer.MemoryCopy(p1, p2, Length, Length);
+                Unsafe.CopyBlock(p3, p1, Length);
+            }
+
+            var memOwner = MemoryPool<byte>.Shared.Rent(Length);
+            var memOwner2 = MemoryPool<byte>.Shared.Rent(Length);
+            var mem = memOwner.Memory;
+            var mem2 = memOwner2.Memory;
+            fixed (byte* p1 = spanSource)
+            {
+                using (var handle = mem.Pin())
+                {
+                    Buffer.MemoryCopy(p1, handle.Pointer, mem.Length, Length);
+                }
+                using (var handle = mem2.Pin())
+                {
+                    Unsafe.CopyBlock(handle.Pointer, p1, Length);
+                }
+            }
+
+            ////Unsafe.CopyBlock ref version
+            //var refArraySrc = new byte[Length];
+            //var refArrayDest = new byte[Length];
+            //new Random(42).NextBytes(refArraySrc);
+            //Unsafe.CopyBlock(ref refArrayDest[0], ref refArraySrc[0], Length);
+
+            display(spanSource);
+            display(dstArray);
+            display(dstArray2);
+            display(mem.Span.Slice(0, Length));
+            display(mem2.Span.Slice(0, Length));
+
+            if (spanSource.SequenceEqual(dstArray) &&
+                spanSource.SequenceEqual(dstArray2) &&
+                spanSource.SequenceEqual(mem.Span.Slice(0, Length)) &&
+               spanSource.SequenceEqual(mem2.Span.Slice(0, Length)) /*&&
+                    new Span<byte>(refArraySrc).SequenceEqual(refArrayDest)*/)
+            {
+                Console.WriteLine("equal");
+            }
+            else
+            {
+                Console.WriteLine("not equal");
+            }
+
+            Marshal.FreeHGlobal(ptr);
+            memOwner.Dispose();
+            memOwner2.Dispose();
+
+            Console.ReadKey();
         }
     }
 }
